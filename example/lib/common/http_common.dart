72dd77b19c917ac887c8d0164b658670489648ca
wb-hy461382 <wb-hy461382@alibaba-inc.com>
2020-11-11 10:08:52 +0800

    feat: add net work utils

---
+++ example/lib/common/http_common.dart
@@ -0,0 +1,8 @@
+class HttpCommon {
+  static const String HTTP_HOST = 'https://alivc-demo.aliyuncs.com/';
+  static const String HTTP_METHOD_GET = 'GET';
+  static const String HTTP_METHOD_POST = 'POST';
+
+  static const String GET_STS = 'demo/getSts';
+  static const String GET_AUTH = 'demo/getVideoPlayAuth';
+}
---
+++ example/lib/util/network_utils.dart
@@ -0,0 +1,36 @@
+import 'package:dio/dio.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_aliplayer_example/common/http_common.dart';
+
+typedef HttpSuccessCallback<T> = void Function(dynamic data);
+
+typedef HttpFailureCallback = void Function(dynamic data);
+
+typedef T JsonParse<T>(String data);
+
+class NetWorkUtils {
+  static final NetWorkUtils _instance = NetWorkUtils._privateConstructor();
+
+  static NetWorkUtils get instance {
+    return _instance;
+  }
+
+  Dio _dio;
+
+  NetWorkUtils._privateConstructor() {
+    if (_dio == null) {
+      _dio = Dio();
+      _dio.options.baseUrl = HttpCommon.HTTP_HOST;
+    }
+  }
+
+  void getHttp(String url) async {
+    Response response = await _dio.get(url);
+    Map<String, dynamic> data = response.data;
+    if (data.isNotEmpty && data['result'] == 'true') {
+      print("success");
+    } else {
+      print('error');
+    }
+  }
+}
